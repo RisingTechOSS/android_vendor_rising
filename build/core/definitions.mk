b8a1fa77e694677a6d76fe46268598ff60c8fffd
Steve Kondik <steve@cyngn.com>
2015-04-05 10:59:21 -0400

    [SQUASH]Import definitions from LOS
    lineage: Move all QCOM board configs to hardware/qcom-caf/common
    
    Change-Id: I6ead35cf884255a8897a3e3e27fc7936d11be616
    build: Separate platform macros from QCOM platform definition
    
    * The macros used in the file formerly called "qcom_utils.mk" are
      actually generic and not QCOM-specific. Move the QCOM_BOARD definitions
      to a QCOM-specific location, and name the remaning macros something more
      generic.
    
    Change-Id: I6747bc9201e56b456caf251122057d7dbc233f1c
    build: Reorganize inclusion of QCOM macros
    
    * There's actually not a whole lot QC-specific about these macros,
      make them available at definition.mk inclusion time rather than
      relying on BoardConfig to include them.
    * Don't create a dependency on QCOM macros from a makefile
      that is included very early in the build process.
    
    Change-Id: I3dcaf0e068ca44732cfe58f69384621781ddda67
    lineage: Complete the rebrand
    
    * TODO: Rebrand cmsdk when it's in a working state
    
    Change-Id: I0d8d573f9e0032110ae3a0f54ebe11a877b83137
    cm: CM-specific build macros
    
     * vendor/*/build/core/definitions.mk is automatically sourced by the
       build system to load custom macros. Start using this for some
       of our own.
     * Adds a "uniq" macro to remove duplicates from a list without changing
       it's order.
    
    Change-Id: Id5f1eb4b9f81bb31ca0b3d5e74c298b3d105da10
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

---
+++ build/core/definitions.mk
@@ -0,0 +1,9 @@
+#
+# Lineage-specific macros
+#
+define uniq
+$(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
+endef
+
+# Include board/platform macros
+include vendor/lineage/build/core/utils.mk
