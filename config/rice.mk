5901c6933f24c92d3eb71322d357738c640edd98
minaripenguin <alexfinhart@gmail.com>
2022-07-13 17:38:48 +0800

    configs: Tune default memory sysprops
    Signed-off-by: minaripenguin <alexfinhart@gmail.com>

--- config/rice.mk
+++ config/rice.mk
@@ -54,15 +54,15 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # perf memory optimizations
 PRODUCT_PRODUCT_PROPERTIES += \
-    persist.sys.fw.bg_apps_limit?=32 \
+    persist.sys.fw.bg_apps_limit?=48 \
     persist.sys.fw.use_trim_settings?=true \
     persist.sys.fw.empty_app_percent?=50 \
     persist.sys.fw.trim_empty_percent?=100 \
     persist.sys.fw.trim_cache_percent?=100 \
     persist.sys.fw.trim_enable_memory?=2147483648 \
-    persist.sys.fw.bservice_age?=5000 \
-    persist.sys.fw.bservice_limit?=5 \
-    persist.sys.fw.bservice_enable?=false
+    persist.sys.fw.bservice_age?=120000 \
+    persist.sys.fw.bservice_limit?=6 \
+    persist.sys.fw.bservice_enable?=true
 
 # Gboard
 PRODUCT_PRODUCT_PROPERTIES += \
