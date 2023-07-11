abed308f59707c8be1122282aa11ca394e1522ae
jhenrique09 <jhsv09@gmail.com>
2023-07-11 20:27:13 -0300

    vendor: Add TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT
    Change-Id: I0b5ded178ec35f87f768509e497ecce4926d4336

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -41,6 +41,7 @@ SOONG_CONFIG_risingGlobalVars += \
     target_health_charging_control_supports_bypass \
     target_health_charging_control_supports_deadline \
     target_health_charging_control_supports_toggle \
+    camera_uses_newer_hidl_override_format \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_surfaceflinger_udfps_lib \
@@ -78,6 +79,7 @@ SOONG_CONFIG_risingNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK
 SOONG_CONFIG_risingQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_risingQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_risingQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_risingGlobalVars_camera_uses_newer_hidl_override_format := $(TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
