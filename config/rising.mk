e699c8922ca8ed2f12831811c3cb2f98ce7bf333
minaripenguin <minaripenguin@users.noreply.github.com>
2023-10-06 11:00:05 +0800

    config: Initial risingOS vendor makefile
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

---
+++ config/rising.mk
@@ -0,0 +1,83 @@
+$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
+
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
+# RisingOS properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.rising.maintainer=$(RISING_MAINTAINER) \
+    ro.rising.code=$(RISING_CODENAME) \
+    ro.rising.packagetype=$(RISING_PACKAGE_TYPE) \
+    ro.rising.releasetype=$(RISING_BUILDTYPE) \
+    ro.rising.version?=$(RISING_VERSION) \
+    ro.rising.chipset?=$(RISING_CHIPSET) \
+    ro.rising.build.version=$(LINEAGE_VERSION) \
+    ro.rising.display.version?=$(LINEAGE_DISPLAY_VERSION) \
+    ro.rising.platform_release_codename=$(RISING_FLAVOR)
+
+# misc properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.input.video_enabled=false \
+    arm64.memtag.process.system_server=off \
+    persist.sys.powerhal.interaction.max=64 \
+    persist.sys.powerhal.interaction.max_default=64 \
+    persist.sys.powerhal.interaction.max_boost=500
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    dalvik.vm.systemuicompilerfilter=speed \
+    persist.sys.strictmode.disable=true
+
+# Blurs
+ifeq ($(TARGET_ENABLE_BLUR), true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+endif
+
+# Freeform
+TARGET_DEVICE_IS_TABLET ?= false
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.wm.debug.desktop_mode=false
+ifeq ($(TARGET_DEVICE_IS_TABLET), true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.wm.debug.desktop_mode.default_width=840 \
+    persist.wm.debug.desktop_mode.default_height=630
+else
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.wm.debug.desktop_mode.default_width=230 \
+    persist.wm.debug.desktop_mode.default_height=360
+endif
+
+# Dexopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Launcher3QuickStep
+
+# Product Copy
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_PRODUCT)/usr/keylayout/Vendor_045e_Product_0719.kl \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml
+
+# /system_ext packages
+PRODUCT_PACKAGES += \
+    androidx.window.extensions \
+    androidx.window.sidecar
+
+# properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.wm.extensions.enabled=true
+
+# vars
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := art/build/boot/boot-image-profile.txt
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+PRODUCT_RESTRICT_VENDOR_FILES := false
