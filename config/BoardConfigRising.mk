ab14e13389ddbda08482647fda0f0da1475d2cbd
Alvin Francis <nivlafx@gmail.com>
2023-11-14 18:11:51 -0400

    Revert "config: Move QC board variables earlier [SQUASH]"
    This reverts commit e0d9913d30b5202c1266b091367bbbb3cd28a3dc.

--- config/BoardConfigRising.mk
+++ config/BoardConfigRising.mk
@@ -1,7 +1,7 @@
 include vendor/rising/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/rising/config/BoardConfigQcom.mk
+include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
 
 include vendor/rising/config/BoardConfigSoong.mk
